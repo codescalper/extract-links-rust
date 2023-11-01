87561313319191f229c17da2f95fca63dc0d3b24
codescalper <msfunbook@gmail.com>
2023-11-01 15:36:25 +0530

    User can give input + comments for better understanding

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::io;
+
 use error_chain::error_chain;
 use select::document::Document;
 use select::predicate::Name;
 
-error_chain! {
+error_chain! { // TO handle std and reqwest error
     foreign_links{
         ReqError(reqwest::Error);
         IoError(std::io::Error);
@@ -11,14 +13,13 @@ error_chain! {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let res = reqwest::get("https://books.10xeng.xyz")
-        .await?
-        .text()
-        .await?;
+    println!("Enter the url of the website: ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read ");
+    let res = reqwest::get(input).await?.text().await?;
     Document::from(res.as_str())
         .find(Name("a"))
-        .filter_map(|n| n.attr("href"))
-        .for_each(|x| println!("{}", x));
-
+        .filter_map(|n| n.attr("href")) // * used closure . it's used to process individual anchor elements
+        .for_each(|x| println!("{}", x)); //*|x| is used to process the values of the "href" attribute extracted from those anchor elements. and pints value to the console */
     Ok(())
 }
