930c84add8733f85479a0ad263f05ba8883521dc
codescalper <msfunbook@gmail.com>
2023-11-01 15:21:31 +0530

    Done with the project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use error_chain::error_chain;
+use select::document::Document;
+use select::predicate::Name;
+
+error_chain! {
+    foreign_links{
+        ReqError(reqwest::Error);
+        IoError(std::io::Error);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let res = reqwest::get("https://books.10xeng.xyz")
+        .await?
+        .text()
+        .await?;
+    Document::from(res.as_str())
+        .find(Name("a"))
+        .filter_map(|n| n.attr("href"))
+        .for_each(|x| println!("{}", x));
+
+    Ok(())
 }
